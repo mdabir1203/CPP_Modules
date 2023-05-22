b3adb7769017dae75aa11d846c8dcc9f8d581352
uKnowWhoab1r <md.abir1203@gmail.com>
2023-05-23 01:38:04 +0200

    ex02 done with tests

--- ex01/Phonebook.cpp
+++ ex01/Phonebook.cpp
@@ -1,76 +1,122 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mabbas <mabbas@students.42wolfsburg.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/11 14:32:32 by mabbas            #+#    #+#             */
-/*   Updated: 2023/05/11 16:58:39 by mabbas           ###   ########.fr       */
+/*   Created: 2023/05/11 01:55:49 by mabbas            #+#    #+#             */
+/*   Updated: 2023/05/18 20:48:29 by mabbas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./includes/Contact.hpp"
+#include "includes/PhoneBook.hpp"
 
-/** Create a constructor -> Call from similar class
-    
- **/
-
-Contact::Contact(){idx = 0;}
-Contact::~Contact(){}
-
-void Contact::addInfo()
+/**
+ * @brief Construct a new Phone Book:: Phone Book object
+ *   
+ */
+Phonebook::Phonebook()
 {
-    std::cout << "First Name: ";
-    _firstName = this->_addInfo("");
-
-    std::cout << "Last Name: ";
-    _lastName  = this->_addInfo("Last Name");
-
-    std::cout << "Nick Name: ";
-    _nickName  = this->_addInfo("Nick Name");
-
-    std::cout << "Phone Number: ";
-    _phoneNumber = this->_addInfo("Phone Number");
-
-    std::cout << "Darkest Secret: ";
-    _darkestSecret = this->_addInfo("Darkest Secret");
-
-    std::cout << "Contact added to the phonebook. \n";
+    contactIndex = 0;
+    oldestContact = 0;
 }
 
-
-void Contact::displayContact() const
+Phonebook::~Phonebook()
 {
-    std::cout << "First Name: " << _firstName << std::endl;
-    std::cout << "Last Name: " << _lastName << std::endl;
-    std::cout << "Nick Name: " << _nickName << std::endl;
-    std::cout << "Phone Number: " << _phoneNumber << std::endl;
-    std::cout << "Darkest Secret: " << _darkestSecret << std::endl;
-    
+    return ;
 }
 
-
-void Contact::displayAll() const
-{
-    std::cout << std::right << std::setw(10) << idx
-              << std::setw(10) << _formatInfo(_firstName)
-              << std::setw(10) << _formatInfo(_lastName)
-              << std::setw(10) << _formatInfo(_nickName)
-              << std::endl;
-}
-
-/** Chose to keep this function due to design patterns 
-    Making a distinction between Public and Private 
+/** 
+    Keeps track of index -> OldContactAddress
+	Once full -> Index increases  -> Wrapped
+	around modulo operator -> Replaces the oldest
+	contact with the new one
+	
 **/
-std::string Contact::_addInfo(std::string text) const
+
+void Phonebook::addInfo()
 {
-    std::string input;
-    std::getline(std::cin >> std::ws, input);
-    return input;
+    if (contactIndex >= 8)
+	{
+        oldestContact = (oldestContact + 1) % 8;
+    }
+    Contact newContact;
+	newContact.addInfo();
+    contacts[contactIndex % 8] = newContact;
+    contactIndex++;
 }
 
-std::string Contact::_formatInfo(std::string info) const
+/** 
+& is here passed by reference not by value
+
+-> Func receives actual memory add of var pass in from calling fnc,instead of copy of its value.
+-> Benefit: Modify value of var directly without separating copy of var.
+        -> Reduce memory usage + improves performance with large obj/arrays
+**/
+
+void Phonebook::getUserInput(bool& displayAllContacts, int& contactIndexToDisplay) const
 {
-    return (info.length() > 10) ? info.substr(0,9) + "." : info;
+	std::cout << "Enter desired index value of the contact or type 'show' to display all contacts:";
+
+	std::string userInput;
+	std::getline(std::cin, userInput);
+	if (userInput == "show")
+		displayAllContacts = true;
+	else
+		contactIndexToDisplay = std::atoi(userInput.c_str());
+}
+
+
+void Phonebook::displayAllContactsInPhonebook() const
+{
+	int i = 0;
+	while (i < contactIndex)
+	{
+		std::cout << "[" << i + 1 << "]";
+		
+		if (contacts[i].displayContact())
+		{
+			std::cout << "Contact is empty" << std::endl;
+		}
+		else
+		{
+			contacts[i].displayContact();
+		}
+		std::cout << std::endl;
+		i++;
+	}
+}
+
+
+/** 
+Supports input validation, retrieves info if it exists and returns
+provides info for incorrect input or out of range indices.
+**/
+
+void Phonebook::search() const
+{
+	if (contactIndex == 0)
+	{
+		std::cout << "No Contacts found\n";
+		return ;
+	}
+
+	bool displayAllContacts = false;
+	int contactIndexToDisplay = -1;
+
+	getUserInput(displayAllContacts, contactIndexToDisplay);
+    
+	if (displayAllContacts)
+	{
+		displayAllContactsInPhonebook();
+	}
+	else
+	{
+		if (contactIndexToDisplay >= 0 && contactIndexToDisplay < contactIndex)
+			contacts[contactIndexToDisplay].displayContact();
+		else
+			std::cout << "Invalid index. Please enter a valid index value," << std::endl;
+	}
+
 }
